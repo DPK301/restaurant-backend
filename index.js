92659244dc604d9705ed05c0e4dd2cf9197319bb
DPK301 <dpkofficial3@gmail.com>
2024-02-16 12:34:12 +0530

    restaurant-backend

---
+++ index.js
@@ -0,0 +1,22 @@
+//1 import json server
+
+const jsonServer = require('json-server');
+//2 create a server application
+const restServer = jsonServer.create()
+
+//setup path for db.json
+const router =  jsonServer.router('db.json')
+
+//returm a middilware instance used by json-server
+const middilware=jsonServer.defaults()
+
+//setup a port  =for db.json
+const port = 3001
+
+//use in server
+restServer.use(middilware)
+restServer.use(router)
+
+//to run the server
+restServer.listen(port,()=>{console.log("rest server listening on the port"+port);})
+
